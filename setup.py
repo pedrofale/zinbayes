8406534f8eef060b381ff1063a20bfe62430aaa0
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-10-27 20:01:03 +0100

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="ZINBayes",
     version="0.1",
     author="Pedro F. Ferreira",
     description="Bayesian Zero-Inflated Negative Binomial Factorization of single-cell RNA-seq data.",
     packages=find_packages(),
+    install_requires=requirements,
 )
